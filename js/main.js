2da0782d82150cd68d3c0086e7c1d1d697533550
Ahmed-Sharkawy <ahmedmaher0110@gmail.com>
2021-11-29 07:29:19 +0200

    add

--- js/main.js
+++ js/main.js
@@ -9,27 +9,29 @@ function calculate(){
 
     let inputBill = document.getElementById('inputBill').value,
     inputPeople = document.getElementById('inputPeople').value;
-    document.getElementById('inputPeople').value = "";
-    document.getElementById('inputBill').value = "";
-
-// start your_service
-
+    
+    // start your_service
+    
     let select = document.getElementById('select_main'),
     sele = select.options[select.selectedIndex].value;
-
-// start button calculate main
-
+    
+    // start button calculate main
+    
     let total = document.getElementById('total');
     if(inputBill == "" || inputPeople == ""){
-
+        
         alert("No field can't be left empty");
-
+        
     }  else {
-
+        
         // total button calculate main
+        
+        output = (inputBill / inputPeople) * sele;
+        output = output.toFixed(2)
+        total.textContent = Math.abs(output)
 
-            output = (inputBill / inputPeople) * sele;
-            total.textContent = Math.abs(output)
+        document.getElementById('inputPeople').value = "";
+        document.getElementById('inputBill').value = "";
     }       
 }
 
